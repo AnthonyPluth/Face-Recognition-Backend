2a4844ccf33a76045ebb3eeaac5e9930c7b52ba4
Anthony Pluth <abpluth@gmail.com>
2020-06-19 15:20:46 -0500

    replace flask with FastAPI, return bboxes instead of image

--- app.py
+++ app.py
@@ -1,32 +1,40 @@
 from face_rec_api import image_processing
 from face_rec_api import training
-from flask import Flask, request
-from flask_cors import cross_origin
-import cv2
-import json
+from fastapi import Path, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import tensorflow as tf
 
-app = Flask(__name__)
+app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class RequestBody(BaseModel):
+    snapshot: str = None
 
 # TODO: make image_processing functions async (again)
 # TODO: switch to sanic/bottle/aiohttp/vibora for async server
 # TODO: switch to websockets?
 
 
-@app.route('/identify', methods=['POST', 'OPTIONS'])
-@cross_origin()
-def find_faces_from_snapshot():
-    print(f'received {request.method} request on /identify endpoint')
-    data = request.get_json()['body']
-    encoded_image = json.loads(data)['snapshot']
-
-    img = image_processing.base64_to_numpy_array(encoded_image)
+@app.post('/identify')
+async def find_faces_from_snapshot(request: RequestBody):
+    img = image_processing.base64_to_numpy_array(request.snapshot)
     faces = image_processing.get_faces(img)
 
     bboxes = []
     for face in faces:
         (x, y, w, h) = face
-        bboxes.append({"x": x, "y": y, "w": w, "h": h})
+        bboxes.append({"x": int(x), "y": int(y), "w": int(w), "h": int(h)})
 
     name, confidence = None, None
     if len(faces) > 0:
@@ -36,43 +44,30 @@ def find_faces_from_snapshot():
     return {'name': name, 'confidence': confidence, "bboxes": bboxes}
 
 
-@app.route('/add_person/<name>', methods=['POST', 'OPTIONS'])
-@cross_origin()
-def add_new_person(name):
-    print(f'received {request.method} request on /add_person endpoint')
-    data = request.get_json()['body']
-    encoded_image = json.loads(data)['snapshot']
-
-    img = image_processing.base64_to_numpy_array(encoded_image)
+@app.post('/add_person/{name}')
+async def add_new_person(request: RequestBody, name: str = Path(..., title="Name of new user")):
+    img = image_processing.base64_to_numpy_array(request.snapshot)
     faces = image_processing.get_faces(img)
+
     bboxes = []
     for face in faces:
         (x, y, w, h) = face
-        bboxes.append({"x": x, "y": y, "w": w, "h": h})
+        bboxes.append({"x": int(x), "y": int(y), "w": int(w), "h": int(h)})
 
     if faces[0]:
         cropped_img = image_processing.crop_frame(img, faces[0])
-
         # save image
         image_processing.save_image(cropped_img, name)
 
-    return {'bboxes': bboxes}
+    return {'name': "RECORDING...", 'confidence': 0.00, "bboxes": bboxes}
 
 
-@app.route('/train_model', methods=['GET', 'OPTIONS'])
-@cross_origin()
-def train_model():
-    print(f'received {request.method} request on /train_model endpoint')
+@app.get('/train_model')
+async def train_model():
     training.run()
     return {'training status': 'complete'}
 
 
-@app.route('/status', methods=['GET', 'OPTIONS'])
-@cross_origin()
-def status():
-    print(f'received {request.method} request on /status endpoint')
+@app.get('/status')
+async def status():
     return {'status': 'up', 'tensorflowVersion': tf.__version__, 'tensorflowGpu': len(tf.config.experimental.list_physical_devices('GPU')) > 0}
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=rue, port=5000, threaded=rue)
