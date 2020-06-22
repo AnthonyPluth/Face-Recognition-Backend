7d840fd422b9bc98ff5bc6a9a494ded6ec3d172d
Anthony Pluth <abpluth@gmail.com>
2020-06-22 10:57:57 -0500

    update dependencies

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ requirements = [
     'numpy>=1.16.4',
     'opencv-python>=4.1.0.25',
     'requests>=2.23.0',
-    'tensorflow==1.15.0',
-    # 'tensorflow-gpu==1.15.0',
+    # 'tensorflow==1.15.0',
+    'tensorflow-gpu==1.15.0',
     'uvicorn>=0.11.5',
 ]
 
