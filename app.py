a57be3bb9423221a18f723e7e186ae06ba89d081
Anthony Pluth <anthony.pluth@target.com>
2020-08-19 15:35:44 -0500

    merge websockets with hassface work

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-# TODO: make image_processing functions async (again)
-# TODO: switch to websockets?
-
 from face_rec_api import image_processing
 from face_rec_api import training
-from fastapi import Path, FastAPI
+from fastapi import File, Path, FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import tensorflow as tf
@@ -19,20 +16,6 @@ app.add_middleware(
 )
 
 
-class RequestBody(BaseModel):
-    snapshot: str = None
-
-
-@app.post("/face/detect")
-async def detect_faces(
-    request: RequestBody
-):
-    img = image_processing.base64_to_numpy_array(request.snapshot)
-    faces, bboxes = get_faces(img)
-
-    return bboxes
-
-
 async def get_faces(img):
     bboxes = []
     faces = image_processing.get_faces(img)
@@ -44,9 +27,15 @@ async def get_faces(img):
     return faces, bboxes
 
 
-@app.post("/face/recognize")
-async def recognize_faces(request: RequestBody):
-    img = image_processing.base64_to_numpy_array(request.snapshot)
+async def detect_faces(snapshot):
+    img = image_processing.base64_to_numpy_array(snapshot)
+    faces, bboxes = get_faces(img)
+
+    return bboxes
+
+
+async def recognize_faces(snapshot: str):
+    img = image_processing.base64_to_numpy_array(snapshot)
     faces, bboxes = get_faces(img)
 
     name, confidence = None, None
@@ -57,11 +46,8 @@ async def recognize_faces(request: RequestBody):
     return {"name": name, "confidence": confidence, "bounding_boxes": bboxes}
 
 
-@app.post("/face/register")
-async def add_new_person(
-    request: RequestBody, name: str = Path(..., title="Name of new user")
-):
-    img = image_processing.base64_to_numpy_array(request.snapshot)
+async def register_face(name, snapshot):
+    img = image_processing.base64_to_numpy_array(snapshot)
     faces, bboxes = get_faces(img)
 
     if faces[0]:
@@ -69,58 +55,66 @@ async def add_new_person(
         # save image
         image_processing.save_image(cropped_img, name)
 
-    return {"name": "RECORDING...", "confidence": 0.00, "bboxes": bboxes}
+    return bboxes
+
+
+@app.post("/face/detect")
+async def detect_faces_post(file: bytes = File(...)):
+    return await detect_faces(file)
+
+
+@app.post("/face/recognize")
+async def recognize_faces_post(file: bytes = File(...)):
+    return await recognize_faces(file)
+
+
+@app.post("/face/register")
+async def register_face(name: str, file: bytes = File(...)):
+    return await register_face(name, file)
 
 
 @app.get("/train_model")
 async def train_model():
     training.run()
-
     # reload encoder with new data
 
     return {"training status": "complete"}
 
 
-@app.get("/status")
-async def status():
-    return {
-        "status": "up",
-        "tensorflowVersion": tf.__version__,
-        "tensorflowGpu": len(tf.config.experimental.list_physical_devices("GPU")) > 0,
-    }
-
-
-def test_webcam():
-    from imutils.video import VideoStream
-
-    cam = VideoStream(src=0).start()
+@app.websocket("/")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
     while True:
-        frame = cam.read()
-        if frame is not None:
-            faces = find_faces_in_frame(frame)
-            for face in faces:
-                name, confidence = identify_person(frame)
-                print(name, confidence)
+        data = await websocket.receive_json()
+        request_type = list(data.keys())[0]
+
+        if request_type == "identify":
+            await recognize_faces(data["identify"])
+        elif request_type == "record":
+            name = request_json["name"]
+            snapshot = request_json["snapshot"]
+            bboxes = await register_face(name, snapshot)
+            return {"name": "RECORDING...", "confidence": 0.00, "bboxes": bboxes}
 
 
-if __name__ == "__main__":
-    import cv2
-
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        ret, img = cap.read()
-        print(img.shape)
-        faces = image_processing.get_faces(img)
-
-        bboxes = []
-        for face in faces:
-            (x, y, w, h) = face
-            bboxes.append({"x": int(x), "y": int(y), "w": int(w), "h": int(h)})
-
-        name, confidence = None, None
-        if len(faces) > 0:
-            cropped_img = image_processing.crop_frame(img, faces[0])
-            name, confidence = image_processing.identify_person(cropped_img)
-
-        print(name, confidence)
+# if __name__ == "__main__":
+#     import cv2
+#
+#     cap = cv2.VideoCapture(0)
+#
+#     while True:
+#         ret, img = cap.read()
+#         print(img.shape)
+#         faces = image_processing.get_faces(img)
+#
+#         bboxes = []
+#         for face in faces:
+#             (x, y, w, h) = face
+#             bboxes.append({"x": int(x), "y": int(y), "w": int(w), "h": int(h)})
+#
+#         name, confidence = None, None
+#         if len(faces) > 0:
+#             cropped_img = image_processing.crop_frame(img, faces[0])
+#             name, confidence = image_processing.identify_person(cropped_img)
+#
+#         print(name, confidence)
